2b282535dce9ec56dd218adbdaea4382f8cec915
Akash Kundu <akashkundu92@gmail.com>
2019-04-29 12:42:35 -0500

    starting to work on unit tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -13,16 +13,91 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testDefaultRandom(type: FormType) {
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    //    case username
+    //    case password
+    //    case email
+    //    case phoneNumber
+    //    case day
+    //    case year
+    //
+    //    case address
+    //    case zip
+    //
+    //    case creditCardNumber
+    //    case creditCardSecurityCode
+    //
+    //    case socialSecurityNumber
+    //
+    //    case driversLicense
+    //    case carLicensePlateNumber
+    //    case carVinNumberPre1981
+    //    case carVinNumberPost1981
+    //
+    //    case badIsbn13
+    
+    
+    func getPredicate(type: FormType) -> NSPredicate {
+        var pattern: String = ""
+        switch type {
+        case .username:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .password:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .email:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .phoneNumber:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .day:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .year:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .address:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .zip:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .creditCardNumber:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .creditCardSecurityCode:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .socialSecurityNumber:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .driversLicense:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .carLicensePlateNumber:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .carVinNumberPre1981:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .carVinNumberPost1981:
+            pattern = "([\\w\\d]){8})"
+            break
+        case .badIsbn13:
+            pattern = "([\\w\\d]){8})"
+            break
         }
+        return NSPredicate(format: "self MATCHES [c] %@", pattern)
     }
-    
 }
+//    if predicate.evaluate(with: "(888) 555-1111") {
+//    print("Valid")
+//    }
+//    else {
+//    print("Invalid")
+//}
